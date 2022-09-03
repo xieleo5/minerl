063055f272226218fcfae9c34b9f658a4dc2568c
xieleo <xieleo@umich.edu>
2022-09-03 13:38:10 -0400

    add getEquippedItem function in java, fix equippedItem not working problem, loads equipped_item in python

--- minerl/herobraine/hero/handlers/agent/observations/equipped_item.py
+++ minerl/herobraine/hero/handlers/agent/observations/equipped_item.py
@@ -137,7 +137,7 @@ class _TypeObservation(TranslationHandler):
         try:
             head = obs_dict['equipped_items']
             for key in self._keys:
-                head = head[key]
+                head = json.loads(head[key])
             item = head['type']
             return (self._other if item not in self._items else item)
         except KeyError:
